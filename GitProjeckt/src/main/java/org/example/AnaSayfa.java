13eeab3aae8c5b37b277457b077d69ff04d405bf
Halil İbrahim Unver <halilibrahimunver58@gmail.com>
2025-02-07 18:28:49 +0300

    new projeckt

---
+++ GitProjeckt/src/main/java/org/example/AnaSayfa.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class AnaSayfa {
+    JFrame frame = new JFrame();
+
+    public AnaSayfa(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Anasayfa");
+        frame.setLayout(null);
+
+
+
+        frame.setVisible(true);
+    }
+}
