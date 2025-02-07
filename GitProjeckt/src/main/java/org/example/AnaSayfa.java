292f27abfac26ca37631d3582283b49e0cda7cd6
Halil İbrahim Unver <halilibrahimunver58@gmail.com>
2025-02-07 18:35:51 +0300

    new projeckt

--- GitProjeckt/src/main/java/org/example/AnaSayfa.java
+++ GitProjeckt/src/main/java/org/example/AnaSayfa.java
@@ -1,18 +1,29 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AnaSayfa {
+public class AnaSayfa implements ActionListener {
     JFrame frame = new JFrame();
+    JButton button = new JButton("Profili Göster");
 
     public AnaSayfa(){
+        button.addActionListener(this);
+        button.setBounds(200,200,100,50);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle("Anasayfa");
         frame.setLayout(null);
 
 
-
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
